1f2bce15611982c863646364e73b7d7c75878c9b
John Sandoval <jcsandoval978@gmail.com>
2023-11-22 14:31:48 -0500

    Added pseudocode to further understand

--- November23/112223TwoSum.js
+++ November23/112223TwoSum.js
@@ -7,7 +7,7 @@
 var twoSum = function (nums, target) {
     // Create for loop to iterate through array
     for (i = 0; i < nums.length; i++) {
-        // Create a second for loop to iterate through the array, one position ahead of the prior loop
+        // Create a second for loop to iterate one position ahead of the prior loop, checking the array one by one while "i" is still in the prior position.
         for (j = i + 1; j < nums.length; j++) {
             // If the sum of the two numbers is equal to the target
             if (nums[i] + nums[j] === target) {
@@ -20,5 +20,5 @@ var twoSum = function (nums, target) {
 
 // Test Cases:
 console.log(twoSum([2,7,11,15], 9)); // --> [0,1]
-console.log(twoSum([3,2,4], 6)); // --> [1,2]
+console.log(twoSum([3,4,0,2], 6)); // --> [1,2]
 console.log(twoSum([3,3], 6)); // --> [0,1]
