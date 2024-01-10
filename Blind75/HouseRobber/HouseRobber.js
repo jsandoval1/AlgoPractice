2d4e67e46db12bcfb25f2157e502e5479bd5dc01
John Sandoval <jcsandoval978@gmail.com>
2024-01-09 19:45:53 -0500

    Started HouseRobber algo

---
+++ Blind75/HouseRobber/HouseRobber.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function (nums) {
+    // Store money in a variable
+    // If "i" is %2 == 0: add this the previous money
+    // Return when done 
+};
